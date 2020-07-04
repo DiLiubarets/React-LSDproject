cb4c672a3ca5e1a645c301dc43f411dac619d0ab
Dina <diana.liubarets@hotmail.com>
2020-07-03 22:57:01 -0400

    Updates

---
+++ precache-manifest.dae4ed0f308c373b2693169bf75a6f66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dbd6316d1bde8aea9dc2e4f99c12274",
+    "url": "/React-LSDproject/index.html"
+  },
+  {
+    "revision": "ef85028cbb568cc98815",
+    "url": "/React-LSDproject/static/css/main.5c66b332.chunk.css"
+  },
+  {
+    "revision": "f29aeace01aa5a0c7016",
+    "url": "/React-LSDproject/static/js/2.a3b12551.chunk.js"
+  },
+  {
+    "revision": "36faa824c91e2ec2a4f703f7b3bb7cc1",
+    "url": "/React-LSDproject/static/js/2.a3b12551.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef85028cbb568cc98815",
+    "url": "/React-LSDproject/static/js/main.1699c962.chunk.js"
+  },
+  {
+    "revision": "2765fced0da0e1b6d453",
+    "url": "/React-LSDproject/static/js/runtime-main.87faf8fe.js"
+  }
+]);
